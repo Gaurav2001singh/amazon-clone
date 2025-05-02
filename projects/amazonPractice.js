52e62b42fd17fcab5184f2259b117ae776961903
Gaurav2001singh <rajs14571@gmail.com>
2025-05-02 18:54:52 +0530

    added files

---
+++ projects/amazonPractice.js
@@ -0,0 +1,38 @@
+// let body = document.getElementsByTagName("header")
+// console.dir(body)
+
+// let divs = document.getElementsByTagName("div");
+// console.dir(divs)
+
+// let para = document.getElementsByTagName("p");
+// console.dir(para)
+
+// let a = document.getElementsByTagName("a");
+// console.dir(a)
+
+// let span = document.getElementsByTagName("span");
+// console.dir(span)
+
+// let h2= document.getElementsByTagName("h2");
+// console.dir(h2)
+
+// let text = document.getElementsByName("text")
+// console.dir(text)
+
+// let div = document.querySelector("div")
+// console.dir(div)
+// let alltel = document.querySelectorAll(".navbar")
+// console.dir(alltel);
+
+// let h2 = document.querySelector("h2")
+// console.dir(h2.innerText)
+// h2.innerText = h2.innerText + " Form Gaurav Singh "
+
+// let divs = document.querySelectorAll("div")
+// let i = 1;
+// for(div of divs){
+//     div.innerText = `New Name ${i}`
+//     i++;
+// }
+// divs[0].innerText = "new name 1st"
+// console.log(divs)
